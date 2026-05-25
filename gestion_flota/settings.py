759c633bc711731e4b2797b4b0569c97945060eb
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2026-05-25 08:28:36 -0400

    ajustes de seguridad

--- gestion_flota/settings.py
+++ gestion_flota/settings.py
@@ -16,11 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gestion-flota-hrn-2025-change-in-production'
+SECRET_KEY = os.getenv('SECRET_KEY', 1234)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
