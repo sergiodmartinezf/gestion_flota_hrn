26eb2739628bc2afa15fea0c481752fb2dcb0134
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2026-06-01 12:48:17 -0400

    eliminación de static (obliatorio usar collectstatic en producción)

--- static/js/listar_proveedores.js
+++
@@ -1,9 +0,0 @@
-function habilitarProveedor(proveedorId, nombreProveedor) {
-    // Configurar el modal
-    document.getElementById('nombreProveedor').textContent = nombreProveedor;
-    document.getElementById('habilitarProveedorForm').action = `/proveedores/habilitar/${proveedorId}/`;
-    
-    // Mostrar el modal
-    const modal = new bootstrap.Modal(document.getElementById('habilitarProveedorModal'));
-    modal.show();
-}
